1809ae869ca7ac5755d2cb128ed7aabfb5b9e9dd
ks-mansi-r <mansi.rupareliya@kalkani.in>
2025-02-07 09:38:28 +0530

    add country data

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -7,22 +7,13 @@ import { Validate } from 'class-validator';
 @Injectable()
 export class CountryService {
   constructor(
-    /**
-     * inject countryRepository
-     */
+    
+    // inject countryRepository
+     
 
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>,
   ) { }
-
-  // public async addCountry(countryData: createCountry) {
-  //   await this.countryRepository.findOne({
-  //     where: { code: countryData.code },
-  //   });
-
-  //   const newCountry = await this.countryRepository.create(countryData);
-  //   return await this.countryRepository.save(newCountry);
-  // }
 
   public async addCountry(countryData: createCountry) {
     await this.countryRepository.findOne({
@@ -32,33 +23,7 @@ export class CountryService {
     const newCountry = await this.countryRepository.create(countryData);
     return await this.countryRepository.save(newCountry);
   }
-  // public async updateCountry(updateCountryDataDto: updateCountry) {
-  //   //find the country
-  //   const existantCountry = await this.countryRepository.findOneBy({
-  //     id: updateCountryDataDto.id,
-  //   });
-
-  //   //update country
-  //   existantCountry.cName = updateCountryDataDto.cName ?? existantCountry.cName;
-  //   existantCountry.code = updateCountryDataDto.code ?? existantCountry.code;
-  //   existantCountry.flag = updateCountryDataDto.flag ?? existantCountry.flag;
-
-  //   //save updated country
-  //   return await this.countryRepository.save(existantCountry);
-  // }
-
-  // public async deleteCountry(id: number): Promise<Country> {
-  //   const country = await this.countryRepository.findOne({
-  //     relations: { timeseries: true },
-  //     where: { id: id },
-  //   });
-  //   if (country.timeseries.length > 0) {
-  //     throw new BadRequestException(
-  //       'This country is not deleted because, it have timeseries data.',
-  //     );
-  //   }
-  //   return await this.countryRepository.remove(country);
-  // }
+  
   public async updateCountry(updateCountryDataDto: updateCountry) {
   // Step 1: Find the existing country
   const existantCountry = await this.countryRepository.findOneBy({
